615a112cc05abdd52b3a829432e204ebe29c95ae
James Paul Turner <jamesturner246@gmail.com>
2021-09-29 14:30:30 +0100

    mesh_pca.py now generates and saves PCA-transformed mesh and markers to *_final files.

--- mesh_pca.py
+++ mesh_pca.py
@@ -2,23 +2,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
+import json
 from stl import Mesh
 from sklearn.decomposition import PCA
 
 
 def main():
+    #prop = 'jpt_mallet'
+    prop = 'jpt_screwdriver'
+    #prop = 'kth_hammer'
+    #prop = 'kth_screwdriver'
+    #prop = 'kth_spanner'
 
-    # # jpt_screwdriver.stl marker coordinates
-    markers1 = np.array([
-	[ 0.0,  78.0,   13.5 ], # handle_1
-	[ 0.0,  78.0,  -13.5 ], # handle_2
-	[ 5.0,  120.0,  0.0  ], # shaft_base
-	[-5.0,  164.0,  0.0  ], # shaft_tip
-    ])
+    # load initial
+    with open(f'./{prop}/{prop}_markers.json', 'r') as f:
+        markers1 = json.load(f)
+        markers1 = np.array([v for v in markers1.values()])
+    mesh1 = Mesh.from_file(f'./{prop}/{prop}_decimated.stl').vectors
 
-    mesh1 = Mesh.from_file('./jpt_screwdriver/jpt_screwdriver_decimated.stl').vectors
-    #mesh1 = Mesh.from_file('./kth_spanner/kth_spanner_decimated.stl').vectors
-
+    # PCA of mesh and markers
     pca = PCA()
     mesh2 = mesh1.reshape(-1, 3)
     mesh2 = pca.fit_transform(mesh2)
@@ -30,12 +32,15 @@ def main():
     markers2 = pca.transform(markers1)
     markers2 -= mesh2_mid
 
-    #print(mesh1[0])
-    #print(mesh2[0])
-
     collection1 = mplot3d.art3d.Poly3DCollection(mesh1, facecolors='red')
     collection2 = mplot3d.art3d.Poly3DCollection(mesh2, facecolors='green')
 
+    # save final
+    with open(f'./{prop}/{prop}_markers_final.json', 'w') as f:
+        json.dump(markers2, f)
+    mesh2.save(f'./{prop}/{prop}_final.stl')
+
+    # plot
     fig = plt.figure()
 
     ax = fig.add_subplot(projection='3d')
@@ -50,8 +55,9 @@ def main():
     ax.set_ylabel('y')
     ax.set_zlabel('z')
 
-    #ax1 = fig.add_subplot(121, projection='3d')
+    # ax1 = fig.add_subplot(121, projection='3d')
     # ax1.add_collection3d(collection1)
+    # ax1.scatter(markers1[:, 0], markers1[:, 1], markers1[:, 2], color='blue')
     # ax1.set_xlim([-150, 150])
     # ax1.set_ylim([-150, 150])
     # ax1.set_zlim([-150, 150])
@@ -59,8 +65,9 @@ def main():
     # ax1.set_ylabel('y')
     # ax1.set_zlabel('z')
 
-    #ax2 = fig.add_subplot(122, projection='3d')
+    # ax2 = fig.add_subplot(122, projection='3d')
     # ax2.add_collection3d(collection2)
+    # ax2.scatter(markers2[:, 0], markers2[:, 1], markers2[:, 2], color='gold')
     # ax2.set_xlim([-150, 150])
     # ax2.set_ylim([-150, 150])
     # ax2.set_zlim([-150, 150])
